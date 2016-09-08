4d825f119219f51912bddfbf4c1bd10c8e23f56e
JiaLei123 <sfw123817@gmail.com>
2016-09-08 16:26:39 +0800

    maopao sort

---
+++ src/BubbleSorter.java
@@ -0,0 +1,34 @@
+import java.util.Comparator;
+
+public class BubbleSorter implements Sorter {
+
+    public <T extends Comparable<T>> void sort(T[] list) {
+        boolean swapped = true;
+        for(int i = 1; i < list.length && swapped; i++) {
+            swapped = false;
+            for(int j = 0; j < list.length - i; j++) {
+                if(list[j].compareTo(list[j+1]) > 0) {
+                    T temp = list[j + 1];
+                    list[j + 1] = list[j];
+                    list[j] = temp;
+                    swapped = true;
+                }
+            }
+        }
+    }
+
+    public <T> void sort(T[] list, Comparator<T> comp) {
+        boolean swapped = true;
+        for(int i = 1; i < list.length; i ++) {
+            swapped = false;
+            for(int j = 0; j < list.length -i; j++) {
+                if(comp.compare(list[j], list[j=1]) > 0) {
+                    T temp = list[j + 1];
+                    list[j + 1] = list[j];
+                    list[j] = temp;
+                    swapped = true;
+                }
+            }
+        }
+    }
+}
--- src/Maopao.java
+++ src/Maopao.java
@@ -1,4 +1,7 @@
 public class Maopao implements IArithmetic{
     public void run() {
+        int[] array = new int[]{2, 3, 4,1,8, 5, 7, 0};
+        Sorter sorter = new BubbleSorter();
+        sorter.sort(array);
     }
 }
